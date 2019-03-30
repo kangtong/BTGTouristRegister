2dc75a1c559a4b8fcf9326e19e190e1b4428ee96
TongKang <kangtong1993@outlook.com>
2019-03-30 12:04:07 +0800

    完善 fragment 主页信息

--- app/src/main/java/com/kangtong/btgtouristregister/view/MainActivity.kt
+++ app/src/main/java/com/kangtong/btgtouristregister/view/MainActivity.kt
@@ -12,29 +12,14 @@ import com.kangtong.btgtouristregister.view.tool.ToolFragment
 import com.kangtong.btgtouristregister.view.tool.dummy.DummyContent
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener, ToolFragment.OnListFragmentInteractionListener {
+class MainActivity : AppCompatActivity(), ToolFragment.OnListFragmentInteractionListener {
     override fun onListFragmentInteraction(item: DummyContent.DummyItem?) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        when (item.itemId) {
-        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation);
-        navigation.setOnNavigationItemSelectedListener(this)
-        setupBottomNavMenu(findNavController(R.id.fragment))
-
-    }
-
-    private fun setupBottomNavMenu(navController: NavController) {
-        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        bottomNav?.setupWithNavController(navController)
+        bottom_navigation.setupWithNavController(findNavController(R.id.fragment))
     }
 }
--- app/src/main/java/com/kangtong/btgtouristregister/view/MainFragment.java
+++ app/src/main/java/com/kangtong/btgtouristregister/view/MainFragment.java
@@ -3,40 +3,72 @@ package com.kangtong.btgtouristregister.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.kangtong.btgtouristregister.R;
+
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * A simple {@link Fragment} subclass.
  */
+// TODO: 2019/3/30 这里有一个切换Fragment就会异常的错误
 public class MainFragment extends Fragment {
 
+    private TextView mTextHello;
+    private Timer mTimer = new Timer();
+    private TimerTask mTimerTask;
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (mTextHello == null) mTextHello = getView().findViewById(R.id.text_hello);
+            switch (msg.what) {
+                case 1:
+                    Date date = new Date();
+                    mTextHello.setText(String.format(getString(R.string.hello_fragment), date, date));
+                    break;
+            }
+        }
+    };
 
     public MainFragment() {
         // Required empty public constructor
     }
 
-    public static MainFragment newInstance() {
-
-        Bundle args = new Bundle();
-
-        MainFragment fragment = new MainFragment();
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_main, container, false);
 
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mTextHello = view.findViewById(R.id.text_hello);
+        mTimerTask = new TimerTask() {
+            @Override
+            public void run() {
+                Message message = new Message();
+                message.what = 1;
+                mHandler.sendMessage(message);
+            }
+        };
+        mTimer.schedule(mTimerTask, new Date(), 1000);
+    }
+
 
 }
