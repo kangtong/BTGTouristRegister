c48a68943a2e1afe43be8c308e22dd6331ee2f3b
DQ <dqdanavera@gmail.com>
2019-03-31 16:49:07 +0800

    feat: 加入 RxJava2 更新 MainFragment.java 定时器

--- app/src/main/java/com/kangtong/btgtouristregister/view/MainFragment.java
+++ app/src/main/java/com/kangtong/btgtouristregister/view/MainFragment.java
@@ -1,15 +1,7 @@
 package com.kangtong.btgtouristregister.view;
 
-
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
-import android.os.Handler;
-import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,67 +11,109 @@ import android.widget.TextView;
 import com.kangtong.btgtouristregister.R;
 import com.kangtong.btgtouristregister.view.guide.GuideActivity;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+import io.reactivex.Flowable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-// TODO: 2019/3/30 这里有一个切换Fragment就会异常的错误
 public class MainFragment extends Fragment {
 
     private TextView mTextHello;
-    private Button mBtnSelectGuide;
-    private Timer mTimer = new Timer();
-    private TimerTask mTimerTask;
-    private Handler mHandler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            if (mTextHello == null) mTextHello = getView().findViewById(R.id.text_hello);
-            switch (msg.what) {
-                case 1:
-                    Date date = new Date();
-                    mTextHello.setText(String.format(getString(R.string.hello_fragment), date, date));
-                    break;
-            }
-        }
-    };
+
+    // 定时器
+    private Disposable interval = null;
+    private MutableLiveData<String> time = new MutableLiveData<>();
 
     public MainFragment() {
-        // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
+    public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
-
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
         mTextHello = view.findViewById(R.id.text_hello);
-        mBtnSelectGuide = view.findViewById(R.id.btn_select_guide);
-        mTimerTask = new TimerTask() {
-            @Override
-            public void run() {
-                Message message = new Message();
-                message.what = 1;
-                mHandler.sendMessage(message);
-            }
-        };
-        mTimer.schedule(mTimerTask, new Date(), 1000);
+
+        Button mBtnSelectGuide = view.findViewById(R.id.btn_select_guide);
         mBtnSelectGuide.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getActivity(), GuideActivity.class));
             }
         });
+
+        // 初始化数据
+        Date date = new Date();
+        String timeFormat = String.format(getString(R.string.hello_fragment), date, date);
+        mTextHello.setText(timeFormat);
+        // 观察者, 数据改变时, 更新 UI
+        time.observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                mTextHello.setText(s);
+            }
+        });
     }
 
+    /**
+     * 开启定时器
+     */
+    private void setupTime() {
+        if (interval == null) {
+            interval = Flowable.interval(1, TimeUnit.SECONDS)
+                    .onBackpressureDrop()
+                    .subscribeOn(Schedulers.computation())
+                    .observeOn(Schedulers.computation())
+                    .subscribe(new Consumer<Long>() {
+                        @Override
+                        public void accept(Long aLong) {
+                            Date date = new Date();
+                            String timeFormat = String.format(getString(R.string.hello_fragment), date, date);
+                            time.postValue(timeFormat);
+                        }
+                    }, new Consumer<Throwable>() {
+                        @Override
+                        public void accept(Throwable throwable) {
+                            throwable.printStackTrace();
+                            if (!interval.isDisposed()) {
+                                interval.dispose();
+                            }
+                        }
+                    });
+        }
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (interval == null) {
+            setupTime();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (!interval.isDisposed()) {
+            interval.dispose();
+        }
+        interval = null;
+    }
 }
