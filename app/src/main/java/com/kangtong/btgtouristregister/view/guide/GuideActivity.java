a030a36334ae238738901a44eaf0440216836e47
TongKang <kangtong1993@outlook.com>
2019-03-31 18:36:16 +0800

    完善读取导游列表

--- app/src/main/java/com/kangtong/btgtouristregister/view/guide/GuideActivity.java
+++ app/src/main/java/com/kangtong/btgtouristregister/view/guide/GuideActivity.java
@@ -11,13 +11,25 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.kangtong.btgtouristregister.R;
 import com.kangtong.btgtouristregister.model.Guide;
 
+import org.litepal.LitePal;
+import org.litepal.crud.LitePalSupport;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class GuideActivity extends AppCompatActivity {
+
+    private ListView listGuide;
+    private TextView textNoneGuide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,19 +37,48 @@ public class GuideActivity extends AppCompatActivity {
         setContentView(R.layout.activity_guide);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        listGuide = findViewById(R.id.list_guide);
+        textNoneGuide = findViewById(R.id.text_none_guide);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
-
+        addGuide();
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(GuideActivity.this, AddGuideActivity.class));
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            }
+        });
+        listGuide.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String string = ((TextView) view).getText().toString();
+                Snackbar.make(view, string, Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
 
+        List<Guide> guideList = LitePal.findAll(Guide.class);
+        List<String> guideNameList = new ArrayList<>();
+        for (Guide guide :
+                guideList) {
+            guideNameList.add(guide.getPeopleName());
+        }
+        if (guideNameList.isEmpty()) {
+            textNoneGuide.setVisibility(View.VISIBLE);
+        } else {
+            textNoneGuide.setVisibility(View.INVISIBLE);
+        }
+        ArrayAdapter<String> mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, guideNameList);
+        listGuide.setAdapter(mAdapter);
+
     }
 
+    private void addGuide() {
+        for (int i = 0; i < 20; i++) {
+            Guide guide = new Guide();
+            guide.setPeopleName("小明" + i + "号");
+            guide.save();
+        }
+    }
 }
