19fc5e8b51adeeb250602b1bbe8aa001f9160e7a
康桐 <kangtong1993@outlook.com>
2019-05-24 18:07:45 +0800

    删除时间同步

--- app/src/main/java/com/kangtong/btgtouristregister/view/MainFragment.java
+++ app/src/main/java/com/kangtong/btgtouristregister/view/MainFragment.java
@@ -8,21 +8,16 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 import com.kangtong.btgtouristregister.R;
 import com.kangtong.btgtouristregister.view.guide.GuideActivity;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Date;
-import java.util.concurrent.TimeUnit;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.MutableLiveData;
-import io.reactivex.Flowable;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -31,9 +26,6 @@ public class MainFragment extends Fragment {
 
     private TextView mTextHello;
 
-    // 定时器
-    private Disposable interval = null;
-    private MutableLiveData<String> time = new MutableLiveData<>();
 
     public MainFragment() {
     }
@@ -54,49 +46,8 @@ public class MainFragment extends Fragment {
 
         // 初始化数据
         Date date = new Date();
-        String timeFormat = String.format(getString(R.string.hello_fragment), date, date);
+        String timeFormat = String.format(getString(R.string.hello_fragment), date);
         mTextHello.setText(timeFormat);
-        // 观察者, 数据改变时, 更新 UI
-        time.observe(this, s -> mTextHello.setText(s));
     }
 
-    /**
-     * 开启定时器
-     */
-    private void setupTime() {
-        if (interval == null) {
-            interval = Flowable.interval(1, TimeUnit.SECONDS)
-                    .onBackpressureDrop()
-                    .subscribeOn(Schedulers.computation())
-                    .observeOn(Schedulers.computation())
-                    .subscribe(aLong -> {
-                        Date date = new Date();
-                        String timeFormat = String.format(getString(R.string.hello_fragment), date, date);
-                        time.postValue(timeFormat);
-                    }, throwable -> {
-                        throwable.printStackTrace();
-                        if (!interval.isDisposed()) {
-                            interval.dispose();
-                        }
-                        interval = null;
-                    });
-        }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (interval == null) {
-            setupTime();
-        }
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        if (!interval.isDisposed()) {
-            interval.dispose();
-        }
-        interval = null;
-    }
 }
